80985925867c8ac6f46a7932b4682f1926774092
Leslie Shea <leslieyshea@gmail.com>
2019-05-13 16:45:03 -0700

    added image and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import './index.css';
 import React from 'react';
 import Header from './Header';
 import Dog from './Dog';
@@ -5,8 +6,9 @@ import Dog from './Dog';
 export default function App() {
   return (
     <>
+      <img src="https://s.abcnews.com/images/Lifestyle/puppy-ht-3-er-170907_12x5_992.jpg"></img>
       <Header />
-      <Dog name="spot" age={5} weight="50 lbs"/>
+      <Dog name="Buddy" age={1} weight="10 lbs"/>
     </>
   );
 }
