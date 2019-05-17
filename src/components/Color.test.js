110a55aff913ed4edd9d3a54d2b31d021e03b2aa
Leslie Shea <leslieyshea@gmail.com>
2019-05-17 15:21:09 -0700

    refactored color and reran tests/passing

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -4,7 +4,7 @@ import Color from './Color';
 
 describe('Color Component', () => {
   it('renders a color', () => {
-    const wrapper = shallow(<Color />);
+    const wrapper = shallow(<Color name="Red" rgb={{ red: 255, green: 0, blue: 0 }} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
